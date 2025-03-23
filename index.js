f0f7d9ebac3a5422900f089b8b52a5604a174e65
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-23 17:00:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// index.js
+
+// Saturday Fun function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Monday Work function (function expression)
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Wrap Adjective function
+  function wrapAdjective(flair = '*') {
+    return function(adj = 'special') {
+      return `You are ${flair}${adj}${flair}!`;
+    };
+  }
+  
+  // Export the functions to make them available for tests
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
